32cc3ce67a9ef5f19328d0ce9492347bf9d8822d
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-12 16:22:06 +0500

    Initial Commit

---
+++ My details/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My details
+//
+//  Created by Dmitry Parhomenko on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
